865c751c9d70569df99ca6f6f53aa79e73c71383
Jason Conger <jason@jasonconger.com>
2021-08-31 12:21:47 -0500

    Update buttercup.py

--- package/bin/buttercup.py
+++ package/bin/buttercup.py
@@ -78,13 +78,6 @@ class BUTTERCUP_INPUT(smi.Script):
                 required_on_create=True
             )
         )
-
-        scheme.add_argument(
-            smi.Argument(
-                'radio',
-                required_on_create=True
-            )
-        )
         
         return scheme
 
